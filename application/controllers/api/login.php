da42815dd9cb6ee05120db35a1feede4676003ad
Alexander Poon <scribbleseq@gmail.com>
2014-03-16 19:32:02 -0400

    "Forgot to add these'

---
+++ application/controllers/api/login.php
@@ -0,0 +1,60 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->model('User_m');
+	}
+
+	/**
+	 * Fallback function if no method is provided. This should always
+	 * error out because this should not ever be called.
+	 */
+	function index()
+	{
+		$arr = array(
+			'status' => array(
+				'status' => 'error',
+				'message' => 'Invalid function.'
+			),
+			'data' => array()
+		);
+	}
+
+	/**
+	 * Function for the app to login with the server. Passed in the
+	 * POST request is the user_id and the access token that the app has
+	 * to have been verified by Facebook at one point.
+	 *
+	 * If the user's User ID exists, then just provide the login infomation
+	 * to the user. Otherwise, then do an FQL query to the user for the users
+	 * name, email address, small and square pictures, and the phone number,
+	 * and do not verify the user. Send a message to the user to verify the
+	 * account, and ask them to provide information.
+	 */
+	function login()
+	{
+		$user_id = $this->request->post('');
+
+
+		$arr = array(
+			'users' => $this->User_m->retrieve(),
+			'pet_name' => 'Cat',
+			'name' => 'Whiskers'
+		);
+
+		echo json_encode($arr);
+	}
+
+	/**
+	 * Function to verify the user. TODO: Figure out what information is being
+	 * sent back.
+	 */
+	function verify()
+	{
+
+	}
+}
---
+++ application/models/fb_users_m.php
@@ -0,0 +1,97 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+Class Fb_users_m extends CI_Model
+{
+	/**
+	 * Retrieve all books.
+	 *
+	 * @return  object
+	 */
+	function retrieve_user($user_id)
+	{
+		$where = array(
+			'user_id' => $user_id
+		);
+
+		$query = $this->db->select('user_id, access_token, full_name, email, pic_square,'
+			. ' pic_small, phone_number, is_verified')
+			->from('fb_users')
+			->where($where)
+			->limit(1);
+
+		return $query->get()->result();
+	}
+
+	/**
+	 * Inserts a new book into the database.
+	 *
+	 * @param   isbn_13
+	 *			The ISBN
+	 *
+	 * @param   title
+	 *			The book's title.
+	 * 
+	 * @param   authors
+	 *			The author(s) (optional)
+	 * 
+	 * @param   publisher
+	 *			The publisher (optional)
+	 * 
+	 * @param   edition
+	 *			The edition of the book (if applicable)
+	 * 
+	 * @param   msrp
+	 *			The retail price (optional)
+	 * 
+	 * @param   year
+	 *			The year the book was published (optional)
+	 * 
+	 * @param   amazon_detail_url
+	 *			The Amazon detail URL (optional)
+	 * 
+	 * @param   amazon_small_image
+	 *			The Amazon Small-sized Image (optional)
+	 * 
+	 * @param   amazon_medium_image
+	 *			The Amazon Medium-sized Image(optional)
+	 * 
+	 * @param   amazon_large_image
+	 *			The Amazon Large-sized Image (optional)
+	 *
+	 * @return  boolean
+	 */
+	function insert($isbn_13, $title, $authors=NULL, $publisher=NULL, $edition=NULL,
+		$msrp=NULL, $year=NULL, $amazon_detail_url=NULL, $amazon_small_image=NULL,
+		$amazon_medium_image=NULL, $amazon_large_image=NULL)
+	{
+		$obj = array(
+			'isbn_13' => $isbn_13,
+			'title' => $title,
+			'authors' => $authors,
+			'publisher' => $publisher,
+			'edition' => $edition,
+			'msrp' => $msrp,
+			'year' => $year
+		);
+
+		$this->db->insert('books', $obj);
+
+		return TRUE;
+	}
+
+	/**
+	 * Inserts a new book into the database. However, the book
+	 * object is passed in. If you do not have certain columns,
+	 * do not include it in the object.
+	 *
+	 * @param   obj
+	 *			The Book Object.
+	 *
+	 * @return  boolean
+	 */
+	function insert_obj($obj)
+	{
+		$this->db->insert('books', $obj);
+
+		return TRUE;
+	}
+}
